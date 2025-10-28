60d9c790f0a888b01b6483b05133235e86fc411d
MatthiasKlossMPZ <m.kloss@schule-mv.de>
2025-10-28 19:24:31 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'lerndashboard-v9'; // ← v9!
+const urlsToCache = [
+  '/',
+  'index.html',
+  'new-resource.html',
+  'manifest.json',
+  'icon-192.png'
+];
+
+// INSTALL
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// ACTIVATE – LÖSCHE ALTEN CACHE
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(names => {
+      return Promise.all(
+        names.filter(name => name !== CACHE_NAME)
+              .map(name => caches.delete(name))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// FETCH – IMMER AKTUELLE VERSION
+self.addEventListener('fetch', event => {
+  if (event.request.url.includes('index.html') || event.request.url.includes('new-resource.html')) {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match(event.request))
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request)
+        .then(response => response || fetch(event.request))
+    );
+  }
+});
