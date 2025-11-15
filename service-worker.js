c8f8dc354151f13b36607cb4f6f7251d1ff687d3
MatthiasKlossMPZ <m.kloss@schule-mv.de>
2025-11-15 23:52:56 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // service-worker.js
-const VERSION = '1.0.3';  // Erhöhe bei jedem Update!
+const VERSION = '1.0.5';  // Erhöhe bei jedem Update!
 const CACHE_NAME = `lerndashboard-v${VERSION.replace(/\./g, '')}`;
 const REPO_PATH = 'https://matthiasklossmpz.github.io/lerndashboard-test/';
 
