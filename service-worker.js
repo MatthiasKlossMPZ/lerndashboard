b1f7c05d8d7461c84c85b6b53213f4d797268643
Matthias Kloß <m.kloss@schule-mv.de>
2026-04-23 22:03:41 +0200

    Neue Version mit Restore

--- service-worker.js
+++ service-worker.js
@@ -22,7 +22,7 @@
 // ================================================
 
 // service-worker.js
-const VERSION = '1.1.42';                     // Nur hier erhöhen bei Änderungen!
+const VERSION = '1.1.44';                     // Nur hier erhöhen bei Änderungen!
 const CACHE_NAME = `lerndashboard-v${VERSION.replace(/\./g, '')}`;
 
 // WICHTIG: Dynamischer Pfad mit garantiertem trailing Slash
