6f018db9e3f98daf932710cd390661b6894d2571
Matthias Kloß <m.kloss@schule-mv.de>
2026-09-18 10:13:51 +0200

    neue Bedieungsanleitung

--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,7 @@
 // Service Worker für Lerndashboard (PWA)
 // ================================================
 
-const VERSION = '1.1.91';                     // ← Bei jedem Deploy hochzählen!
+const VERSION = '1.1.92';                     // ← Bei jedem Deploy hochzählen!
 const CACHE_NAME = `lerndashboard-v${VERSION.replace(/\./g, '')}`;
 
 const REPO_PATH = (() => {
